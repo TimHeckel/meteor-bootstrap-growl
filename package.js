b6366afc6eac35e31a0a16f4437da732d4196068
Duc Duong <duongthienduc@yahoo.com>
2014-09-29 17:14:43 +0700

    Update package.js and read file

--- package.js
+++ package.js
@@ -1,10 +1,19 @@
 Package.describe({
-    summary: "Pretty simple jQuery plugin that turns standard Bootstrap alerts into Growl-like notifications."
+	summary: "Wrapper of meteor-bootstrap-growl package that supports Meteor 0.9",
+	"version": "1.0.2",
+	"git": "https://github.com/duongthienduc/meteor-bootstrap-growl.git",
+	"name": "duongthienduc:meteor-bootstrap-growl"
 });
 
-Package.on_use(function (api) {
-    api.use('jquery', 'client');
-    api.add_files([
-        'lib/jquery.bootstrap-growl.js'
-    ], 'client');
-});
+if (typeof Package.onUse == "function") { // for meteor v0.9+
+	Package.onUse(function(api) {
+		api.versionsFrom('METEOR@0.9.0');
+		api.use(['jquery'], 'client');
+		api.addFiles('lib/jquery.bootstrap-growl.js', 'client');
+	});
+} else { // for meteor < v0.9
+	Package.on_use(function(api) {
+		api.use('jquery', 'client');
+		api.add_files('lib/jquery.bootstrap-growl.js', 'client');
+	});
+}
