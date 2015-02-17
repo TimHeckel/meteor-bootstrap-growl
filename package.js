b98c59e9bb185852d94145fd00941317cc61f2dd
Tim Heckel <tim@slatebox.com>
2015-02-17 15:22:16 -0600

    Upgrading to Meteor 1.0

--- package.js
+++ package.js
@@ -1,9 +1,11 @@
 Package.describe({
-    summary: "Pretty simple jQuery plugin that turns standard Bootstrap alerts into Growl-like notifications."
+	name: "mrt:bootstrap-growl"
+	, version: "0.1.4"
+    , summary: "Pretty simple jQuery plugin that turns standard Bootstrap alerts into Growl-like notifications."
 });
 
 Package.on_use(function (api) {
-    api.use('jquery', 'client');
+    api.use('jquery@1.0.11', 'client');
     api.add_files([
         'lib/jquery.bootstrap-growl.js'
     ], 'client');
